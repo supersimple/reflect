6d169008622582838ef5976e16489d089c1fa0e3
Todd Resudek <toddresudek@gmail.com>
2018-10-10 22:20:44 -0600

    update data every 5 minutes

--- lib/reflect/recur.ex
+++ lib/reflect/recur.ex
@@ -28,6 +28,6 @@ defmodule Reflect.Recur do
   end
 
   defp schedule_work() do
-    Process.send_after(self(), :work, 2 * 60 * 1000)
+    Process.send_after(self(), :work, 5 * 60 * 1000)
   end
 end
