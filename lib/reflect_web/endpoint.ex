138ea1bd6813674421865d8277fa6ec13a6b8ab3
Todd Resudek <toddresudek@gmail.com>
2019-05-10 19:19:05 -0600

    Change json parser to Jason

--- lib/reflect_web/endpoint.ex
+++ lib/reflect_web/endpoint.ex
@@ -32,7 +32,7 @@ defmodule ReflectWeb.Endpoint do
   plug(Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
-    json_decoder: Poison
+    json_decoder: Jason
   )
 
   plug(Plug.MethodOverride)
