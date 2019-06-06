ca5407ac7c528438c9e3c676bd4b730b771fdd86
Todd Resudek <toddresudek@gmail.com>
2019-06-06 09:44:47 -0600

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Reflect.Mixfile do
   def project do
     [
       app: :reflect,
-      version: "2.0.0",
+      version: "2.0.1",
       elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
