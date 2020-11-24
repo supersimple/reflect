2f6a3f5e2772c5a7c6b049113ad719964ffadb91
Todd Resudek <toddresudek@gmail.com>
2020-11-23 22:42:50 -0700

    require elixir 1.9+

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Reflect.MixProject do
     [
       app: :reflect,
       version: "3.0.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
